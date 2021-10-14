230422f3ee3f31171aa78175ad9227b2b33f7409
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-10-14 12:51:02 +0200

    added empty Spring Boot project

---
+++ src/main/java/com/ppkwu/lab1/PpkwuLab1Application.java
@@ -0,0 +1,13 @@
+package com.ppkwu.lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkwuLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpkwuLab1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ppkwu/lab1/PpkwuLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppkwu.lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpkwuLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
