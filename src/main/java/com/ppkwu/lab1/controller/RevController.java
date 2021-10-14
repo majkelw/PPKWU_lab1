e73ad3856c4cc9087d68ba2ee302102e30091067
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-10-14 13:02:55 +0200

    added request parameter for GET, return not reversed string

--- src/main/java/com/ppkwu/lab1/controller/RevController.java
+++ src/main/java/com/ppkwu/lab1/controller/RevController.java
@@ -1,13 +1,14 @@
 package com.ppkwu.lab1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RevController {
 
     @GetMapping("/api/test")
-    public String test(){
-        return "test";
+    public String reverseString(@RequestParam(name = "str") String str){
+        return str;
     }
 }
